400e26a4ce0ac4f2664d584a12b563b7b6fc8093
haseena <haseena03@gmail.com>
2023-04-03 16:18:16 +0530

    user posts application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [users, setUsers] = useState("");
+  const [posts, setPosts] = useState("");
+
+
+  //GET USERS
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      // console.log(data);
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //GET POSTS
+const handlePosts= async () => {
+     try {
+       const { data } = await axios.get(
+         "https://jsonplaceholder.typicode.com/posts"
+       );
+       // console.log(data);
+       setPosts(data);
+     } catch (error) {
+       console.log(error);
+     }
+   };
+
+
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+        <h1 className="text-center">Post Users App</h1>
+        <button className="btn btn-success" onClick={handlePosts}>
+          Load Posts
+        </button>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+          {posts &&
+            posts.map((post) => (
+              <div className="card p-3 mt-2" key={post.userId}>
+                <p>{post.id}</p>
+                <p>{post.title}</p>
+                <p>{post.body}</p>
+              </div>
+            ))}
+           </div>
+<div className="col-md-6">
+              {users &&
+                users.map((user) => (
+                  <div className="card p-3 mt-2" key={user.id}>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                    <p>{user.phone}</p>
+                  </div>
+                ))}
+            </div>
+          </div>
+      </div>
     </div>
+    </>
   );
 }
-
 export default App;
